269929344c589da1d561b8beeaabb03f0a9a8102
Akshay <akshay98322@gmail.com>
2021-01-12 21:32:01 +0530

    Added GenericAPIView

--- api_basic/views.py
+++ api_basic/views.py
@@ -2,14 +2,36 @@ from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 from .models import Article
 from .serializers import ArticleSerializer
-from django.views.decorators.csrf import csrf_exempt
+# from django.views.decorators.csrf import csrf_exempt
 
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status,generics,mixins
 
 # Create your views here.
+
+class GenericAPIView(generics.GenericAPIView,mixins.ListModelMixin, mixins.CreateModelMixin, mixins.UpdateModelMixin,mixins.RetrieveModelMixin,mixins.DestroyModelMixin):
+    serializer_class = ArticleSerializer
+    queryset = Article.objects.all()
+
+    lookup_field = 'id'
+
+    def get(self,request, id=None):
+        if id:
+            return self.retrieve(request)
+        else:
+            return self.list(request)
+
+    def post(self,request):
+        return  self.create(request)
+
+    def put(self,request,id =None):
+        return self.update(request,id)
+
+    def delete(self,request,id):
+        return self.destroy(request,id)
+
 
 
 class ArticleAPIView(APIView):
