57edd9c51c1d0352c1f260ed319637c6bf5cb7c8
Akshay <akshay98322@gmail.com>
2021-01-12 12:57:32 +0530

    Added delete and update functionality

--- MyProject/settings.py
+++ MyProject/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api_basic.apps.ApiBasicConfig',
+    'api_basic',
     'rest_framework',
 ]
 
